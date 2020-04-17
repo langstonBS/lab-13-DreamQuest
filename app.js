e6ad44301b1eacc2414f207d1cc3b0e1d693376e
langstonBS <langston.beckwithstanley@gmail.com>
2020-04-17 11:16:07 -0700

    form started

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const myForm = document.getElementById('user-form');
 myForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const formData = new FormData(myForm);
-    console.log(formData.get('class'), formData.get('name'));
+    console.log(formData.get('class'),formData.get('name'));
 });
